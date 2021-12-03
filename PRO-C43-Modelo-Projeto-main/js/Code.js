463ec8fefe18136de200c3d803dabb18b0157d0c
V. Croce <69215919+obelisco45@users.noreply.github.com>
2021-12-03 18:28:27 -0300

    Add files via upload

---
+++ PRO-C43-Modelo-Projeto-main/js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Dica: Sempre mudando, não constante!!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 700,150)
+    fill("lightblue")
+    text("Dica: Executa uma tarefa específica!!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,250)
+    fill("lightblue")
+    text("Dica: Armazena todas as informações!!", 100,270)
+    
+
+}
